7758d2ad3d694c8b565218070c1926e9b23c87dc
Adrian Georgita <adrian.georgita@student.tuiasi.ro>
2024-05-14 17:17:49 +0300

    Testing

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace proiect_ip
         [STAThread]
         static void Main()
         {
+            // Comentariu
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main_Menu());
